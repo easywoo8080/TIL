bf7c64651506a0fdf383d01eca6bfe4887f97a6b
ziuzi808 <ziuzi808@gmail.com>
2022-05-25 18:12:11 +0900

    2022-05-25 muti

--- mlp/html/day05/src/main/java/com/day05/day05/controller/AJAXController.java
+++ mlp/html/day05/src/main/java/com/day05/day05/controller/AJAXController.java
@@ -83,4 +83,52 @@ public class AJAXController {
 
         return ja;
     }
+
+
+    @RequestMapping("/getmarkers")
+    public Object getmarkers(String loc){
+        JSONArray ja = new JSONArray();
+        if( loc.equals("s")){
+            JSONObject jo1 = new JSONObject();
+            jo1.put("content", "<div>카카오</div>");
+            jo1.put("lat", 37.55041692365908);
+            jo1.put("lng", 126.91037178013711);
+            jo1.put("target", "js01");
+            ja.add(jo1);
+            JSONObject jo2 = new JSONObject();
+            jo2.put("content", "<div>생태연못</div>");
+            jo2.put("lat", 37.55021692365908);
+            jo2.put("lng", 126.91027178013711);
+            jo2.put("target", "js02");
+            ja.add(jo2);
+            JSONObject jo3 = new JSONObject();
+            jo3.put("content", "<div>슈퍼마켓</div>");
+            jo3.put("lat", 37.55061692365908);
+            jo3.put("lng", 126.91237178013711);
+            jo3.put("target", "js03");
+            ja.add(jo3);
+        }else if(loc.equals("b")){
+            JSONObject jo1 = new JSONObject();
+            jo1.put("content", "<div>부산대</div>");
+            jo1.put("lat", 35.17442453774257);
+            jo1.put("lng", 129.16869784099807);
+            jo1.put("target", "js04");
+            ja.add(jo1);
+            JSONObject jo2 = new JSONObject();
+            jo2.put("content", "<div>부산동산</div>");
+            jo2.put("lat", 35.17142453774257);
+            jo2.put("lng", 129.16769784099807);
+            jo2.put("target", "js05");
+            ja.add(jo2);
+            JSONObject jo3 = new JSONObject();
+            jo3.put("content", "<div>부산아파트</div>");
+            jo3.put("lat", 35.17422453774257);
+            jo3.put("lng", 129.16569784099807);
+            jo3.put("target", "js06");
+            ja.add(jo3);
+        }else if(loc.equals("k")){
+            
+        }
+        return ja;
+    }
 }
